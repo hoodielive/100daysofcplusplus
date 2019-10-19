20ba3104467b3f277272e332c394ea5520d23f17
hoodielive <hoodielive@gmail.com>
2019-10-19 11:27:23 -0400

    day 3 forgot to upload

---
+++ Day3/cmake-build-debug/CMakeFiles/Day3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Day3.dir/main.cpp.o"
+  "Day3.pdb"
+  "Day3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Day3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Day3/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int f(int i) {
+    bool b1 {i!=0};
+}
+
+int main()
+{
+   int a = f(3);
+   std::cout << a << std::endl;
+}
